b3fb136ad60db7da52859befe87d0fed85302d73
Hoàng Hữu Thành <165276961+hoanghuuthanhdev@users.noreply.github.com>
2025-02-16 13:27:04 +0700

    Update 0088-merge-sorted-array.java

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -8,9 +8,24 @@ class Solution {
           
     }
     // // Solution2
-    // public void merge2(int[] nums1, int m, int[] nums2, int n){
-
-    // }
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums2[p2] > nums1[p1]) {
+                nums1[p--] = nums2[p2];
+                p2--;
+            } else {
+                nums1[p--] = nums1[p1];
+                p1--;
+            }
+        }
+        while(p2>=0){
+            nums1[p--]=nums2[p2];
+            p2--;
+        }
+    }
 
     
 }
